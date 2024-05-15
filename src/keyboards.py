50c71d46923d1ff9ecee29f4ac78446b8e5b92d3
Aleksey_Pahalev <apahalev@gmail.com>
2024-05-15 22:52:10 +0400

    Update keyboards.py

--- src/keyboards.py
+++ src/keyboards.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 subscription_keyboard = InlineKeyboardMarkup(row_width = 1, inline_keyboard=[
-    [InlineKeyboardButton(text="Подписаться", url="https://t.me/test_chanel_46")],
+    [InlineKeyboardButton(text="Подписаться", url="https://t.me/barrel_token")],
     [InlineKeyboardButton(text="Проверить подписку", callback_data="check_subscription")],
 ])
 
